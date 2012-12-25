b5867cb4248d228ce748a580d18647c87dc91016
Le Duc Duy <me@duy.kr>
2012-12-26 03:05:06 +0700

    Support universal language

--- lib/mongoid-localized-accessor.rb
+++ lib/mongoid-localized-accessor.rb
@@ -4,13 +4,15 @@ module Mongoid
   module Localized
     module Accessor
       def method_missing method
-        if (org = method.to_s.split(/_\w\w$/)).length == 2
-          if self.respond_to? org[0].to_sym
-            if self.try(:[],org[0].to_sym)
-              self.try(:[],org[0].to_sym).try(:[],org[1])
+        if (match_data = method.to_s.match(/(?<method>.*)_(?<lang>\w\w)/))
+          method = match_data['method']
+          lang = match_data['lang']
+          if self.respond_to? method.to_sym
+            if self.try(:[],method.to_sym)
+              self.try(:[],method.to_sym).try(:[],lang)
             else
-              if self.try(org[0].to_sym)
-                self.try(org[0].to_sym).try(:[],org[1])
+              if self.try(method.to_sym)
+                self.try(method.to_sym).try(:[],lang)
               end
             end
           else
--- lib/mongoid-localized-accessor/version.rb
+++ lib/mongoid-localized-accessor/version.rb
@@ -1,7 +1,7 @@
 module Mongoid
   module Localized
     module Accessor
-      VERSION = "0.0.1"
+      VERSION = "0.1.0"
     end
   end
 end
